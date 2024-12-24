90538e0c818894266c0b90c0559b4c1627333f50
RamaSandhy <rshandy07@gmail.com>
2024-12-24 16:35:42 +0700

    Menambahkan kerangka awal untuk TambahDokterViewModel

---
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/TambahDokterViewModel.kt
@@ -0,0 +1,8 @@
+package com.example.ucp2pam.ui.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.ucp2pam.data.repository.DoctorRepository
+
+class TambahDokterViewModel(private val doctorRepository: DoctorRepository) : ViewModel() {
+
+}
