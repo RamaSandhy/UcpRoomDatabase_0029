3bf4e4084eb46a2d25853cdd9b7e28128abec216
RamaSandhy <rshandy07@gmail.com>
2024-12-24 16:37:15 +0700

    Menambahkan fungsi tambahDokter di TambahDokterViewModel

--- app/src/main/java/com/example/ucp2pam/ui/viewmodel/TambahDokterViewModel.kt
+++ app/src/main/java/com/example/ucp2pam/ui/viewmodel/TambahDokterViewModel.kt
@@ -1,8 +1,21 @@
 package com.example.ucp2pam.ui.viewmodel
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.ucp2pam.data.entity.Dokter
 import com.example.ucp2pam.data.repository.DoctorRepository
+import kotlinx.coroutines.launch
 
 class TambahDokterViewModel(private val doctorRepository: DoctorRepository) : ViewModel() {
 
+    fun tambahDokter(dokter: Dokter, onSuccess: () -> Unit, onError: (Exception) -> Unit) {
+        viewModelScope.launch {
+            try {
+                doctorRepository.insert(dokter)
+                onSuccess()
+            } catch (e: Exception) {
+                onError(e)
+            }
+        }
+    }
 }
