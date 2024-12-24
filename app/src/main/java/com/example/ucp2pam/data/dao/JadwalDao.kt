e46ba314054fbaf542e59b0a09df4f34477e7da2
RamaSandhy <rshandy07@gmail.com>
2024-12-24 11:55:06 +0700

    Tambahkan metode insertJadwal ke JadwalDao

---
+++ app/src/main/java/com/example/ucp2pam/data/dao/JadwalDao.kt
@@ -0,0 +1,15 @@
+package com.example.ucp2pam.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+import com.example.ucp2pam.data.entity.Jadwal
+
+@Dao
+interface JadwalDao {
+    @Insert
+    suspend fun insertJadwal(jadwal: Jadwal)
+
+
