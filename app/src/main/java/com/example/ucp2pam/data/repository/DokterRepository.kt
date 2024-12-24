801894222bffe7fd69e5fb53c5d7bd51faa76f97
RamaSandhy <rshandy07@gmail.com>
2024-12-24 15:38:12 +0700

    mengganti package dari pertemuan 9 ke ucp2pam

--- app/src/main/java/com/example/ucp2pam/data/repository/DokterRepository.kt
+++ app/src/main/java/com/example/ucp2pam/data/repository/DokterRepository.kt
@@ -1,6 +1,6 @@
 package com.example.ucp2pam.data.repository
 
-import com.example.pertemuan9.data.entity.Dokter
+import com.example.ucp2pam.data.entity.Dokter
 import com.example.ucp2pam.data.dao.Dokterdao
 
 class DoctorRepository(private val DokterDao: Dokterdao) {
